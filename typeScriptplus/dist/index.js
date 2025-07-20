8e3c22a7732b746cbe6746866dfe783cf3b2334a
codennomad <gabrielferreiralockvieira@gmail.com>
2025-07-20 11:09:32 -0300

    feat: Configure TypeScript for Node.js project
    This commit introduces TypeScript into the Node.js project, enhancing code quality, maintainability, and developer experience through static typing.
    
    Key benefits and changes include:
    - Setup of 	sconfig.json with appropriate compiler options for Node.js development.
    - Integration of TypeScript into the build process, enabling compilation of .ts files to JavaScript.
    - Improved code readability and reduced runtime errors through type checking.
    - Enhanced IDE support with auto-completion and navigation for better developer productivity.
    - Paving the way for better scalability and long-term project health.

---
+++ typeScriptplus/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function main() {
+    let myEmail = "Teste@test.com";
+    console.log(myEmail);
+}
+main();
---
+++ typeScriptplus/src/index.ts
@@ -0,0 +1,6 @@
+function main(){
+    let myEmail: string = "Teste@test.com";
+    console.log(myEmail)
+}
+
+main();
